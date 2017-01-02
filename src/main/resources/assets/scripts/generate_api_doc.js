5bb63c67b2f2ea480fcee9c33834a212dd546c9c
Claas Ahlrichs <claasahl@gmail.com>
2017-01-02 16:57:27 +0100

    simplified generation of API documentation

--- src/main/resources/assets/scripts/generate_api_doc.js
+++ src/main/resources/assets/scripts/generate_api_doc.js
@@ -1,28 +1,22 @@
 // loading modules
 var raml2html = require('raml2html');
-var writeFile = require('write');
 var cmdArgs = require('command-line-args');
-var exit = require('exit');
-
-// called in case of an error 
-var logError = function(error) {
-	if (error)
-		console.error(error);
-	exit(-1);
-};
 
 // retrieving command line arguments
 var optionDefinitions = [ {
 	name : 'src',
-	type : String
-}, {
-	name : 'dst',
-	type : String
+	type : String,
+	defaultOption : true
 } ];
 var options = cmdArgs(optionDefinitions);
 
 // generate API documentation based on supplied RAML file
 var configWithDefaultTemplates = raml2html.getDefaultConfig();
-raml2html.render(options.src, configWithDefaultTemplates).then(function(result) {
-	writeFile(options.dst, result, logError);
-}, logError);
+raml2html.render(options.src, configWithDefaultTemplates).then(
+		function(result) {
+			console.log(result);
+		}, function(error) {
+			if (error)
+				console.error(error);
+			process.exitCode = -1;
+		});
