4ecdf76cff84a8e1dcfb5f51403fb00a1cca5269
Claas Ahlrichs <claasahl@gmail.com>
2017-01-03 15:19:07 +0100

    install bootstrap in public html directory

--- src/main/resources/assets/scripts/copy_bootstrap.js
+++ src/main/resources/assets/scripts/copy_bootstrap.js
@@ -4,3 +4,7 @@ var util = require('utilities');
 var src = 'node_modules/bootstrap/dist';
 var dst = pkg.config.baseDir.public;
 util.file.cpR(src, dst);
+
+src = pkg.config.baseDir.private + '/html';
+dst = pkg.config.baseDir.public;
+util.file.cpR(src, dst);
